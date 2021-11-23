4a7a7b6678ff39b3c8649d9b3fb73ac3015be452
Fabrizzio De Bracamonte <fabrizziodbc@gmail.com>
2021-11-23 08:31:35 -0500

    done

--- src/App.js
+++ src/App.js
@@ -1,41 +1,58 @@
-import React, { Component } from 'react';
+import React, { useState } from "react";
 
 /// Mostrar una lista en base a un arreglo
 
-class App extends Component {
+/* class App extends Component {
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
-      list: ['Javascript', 'CSS', 'HTLM', 'React']
-    }
+      list: ["Javascript", "CSS", "HTLM", "React"],
+    };
   }
   render() {
     return (
       <div style={styles.container}>
         <h1 style={styles.title}>Las tecnologias del Front</h1>
         <ul style={styles.list}>
-          {/* Tu codigo va aca */}
+          {this.state.list.map((e) => (
+            <li key={e}>{e}</li>
+          ))}
         </ul>
       </div>
     );
   }
-}
+} */
+
+
+const App = () => {
+  const [list] = useState(["Javascript", "CSS", "HTLM", "React"]);
+  return (
+    <div style={styles.container}>
+      <h1 style={styles.title}>Las tecnologias del Front</h1>
+      <ul style={styles.list}>
+        {list.map((e) => (
+          <li key={e}>{e}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 const styles = {
   container: {
-    height: '100vh',
-    display: 'flex',
-    flexDirection: 'column',
-    justifyContent: 'center',
-    alignItems: 'center'
+    height: "100vh",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   },
   title: {
-    marginTop: '100px'
+    marginTop: "100px",
   },
   list: {
-    fontSize: '20px',
-    flexGrow: 6
-  }
-}
+    fontSize: "20px",
+    flexGrow: 6,
+  },
+};
 
 export default App;
